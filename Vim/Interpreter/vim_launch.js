19a3a7de11ea5d4a2852d6f579f952122b3259af
tinmarino <tinmarino@gmail.com>
2017-11-01 15:37:29 +0100

    Vim:push all human code in vim_launch (include run_withFs

--- Vim/Interpreter/vim_launch.js
+++ Vim/Interpreter/vim_launch.js
@@ -32,6 +32,60 @@ function tin_download(file, content){
     dl.click();
 }
 
+
+var Module;
+if(typeof Module==="undefined")Module=eval("(function() { try { return Module || {} } catch(e) { return {} } })()");
+if(!Module.expectedDataFileDownloads){
+	Module.expectedDataFileDownloads=0;
+	Module.finishedDataFileDownloads=0}
+Module.expectedDataFileDownloads++;
+
+
+((function(){function runWithFS(){function assert(check,msg){if(!check)throw msg+(new Error).stack}Module["FS_createPath"]("/","usr",true,true);
+
+
+Module["FS_createPath"]("/usr","local",true,true);
+Module["FS_createPath"]("/usr/local","share",true,true);
+Module["FS_createPath"]("/usr/local/share","vim",true,true);
+Module["FS_createPath"]("/usr/local/share/vim","syntax",true,true);
+Module["FS_createPath"]("/usr/local/share/vim","colors",true,true);
+Module["FS_createPath"]("/usr/local/share/vim","doc",true,true);
+Module["FS_createPath"]("/usr/local/share/vim","vimfiles",true,true);
+Module["FS_createPath"]("/usr/local/share/vim/vimfiles","doc",true,true);
+Module["FS_createPath"]("/usr/local/share/vim","cheatsheet",true,true);
+Module["FS_createPath"]("/usr/local/share/vim/cheatsheet","doc",true,true);
+
+
+
+
+
+// Yu heritage
+tin_load("usr/local/share/vim", "vimrc");
+tin_load("usr/local/share/vim/syntax","synload.vim");
+tin_load("usr/local/share/vim/syntax","syntax.vim");
+tin_load("usr/local/share/vim/syntax","javascript.vim");
+tin_load("usr/local/share/vim/syntax","nosyntax.vim");
+tin_load("usr/local/share/vim/syntax","vim.vim");
+tin_load("usr/local/share/vim/colors","Darkside.vim");
+
+// Tin heritage
+tin_load("usr/local/share/vim", "first-page.txt");
+tin_load("usr/local/share/vim/cheatsheet/doc", "c-meta.txt");
+tin_load("usr/local/share/vim/syntax", "myhelp.vim");
+tin_load("usr/local/share/vim/syntax", "help.vim");
+tin_load("usr/local/share/vim/syntax", "sh.vim");
+tin_load("usr/local/share/vim/syntax", "python.vim");
+// <-- Tinmarino 
+
+
+
+// End from Yu
+}if(Module["calledRun"]){runWithFS()}else{if(!Module["preRun"])Module["preRun"]=[];
+Module["preRun"].push(runWithFS)}}))();
+
+
+
+
+
 // Include the baby 
 document.write('<script type="text/javascript" src="vim.js" ></script>')
-
