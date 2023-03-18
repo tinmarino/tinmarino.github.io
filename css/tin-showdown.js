800b8c36a9f4251c2f83b3c56021f56e4be4fda8
Tinmarino <tinmarino@gmail.com>
2023-03-18 17:06:06 -0300

    Css: Add some styles

--- css/tin-showdown.js
+++ css/tin-showdown.js
@@ -1,8 +1,26 @@
-function run() {
-  var text = document.getElementById('sourceTA').value,
-      target = document.getElementById('targetDiv'),
-      converter = new showdown.Converter(),
-      html = converter.makeHtml(text);
+
+async function onLoad() {
+  console.log('Loading is called');
+
+  // Comment out, gets this is undefined, must load in html header
+  //await import('https://cdnjs.cloudflare.com/ajax/libs/showdown/2.1.0/showdown.min.js')
+
+  convertShowdown()
+}
+
+
+function convertShowdown() {
+  console.log('Showdown is converting page')
+
+  var converter = new showdown.Converter();
+
+  var source = document.body.innerText;
+  var target = document.body;
+
+  var html = converter.makeHtml(source);
     
   target.innerHTML = html;
 }
+
+
+window.onload = onLoad;
