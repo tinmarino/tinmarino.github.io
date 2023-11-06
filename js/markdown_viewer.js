54706ee4ffdf9711fe31d55980024543f4089551
Tinmarino <tinmarino@gmail.com>
2023-11-06 18:54:07 -0300

    Js: fix add slugify function

--- js/markdown_viewer.js
+++ js/markdown_viewer.js
@@ -452,4 +452,18 @@ function getPageLocation(){
   return page
 }
 
+
+function slugify(text){
+  return text
+    .toString()
+    .normalize('NFD')
+    .replace(/[\u0300-\u036f]/g, '')
+    .toLowerCase()
+    .trim()
+    .replace(/\s+/g, '-')
+    .replace(/[^\w-]+/g, '')
+    .replace(/--+/g, '-')
+}
+
+
 window.onload = onLoad;
